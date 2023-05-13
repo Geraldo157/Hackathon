b715853977c41a67eb975b2a1f598b82c4cfeff3
Geraldo157 <gsmecanica76@gmail.com>
2023-05-13 06:10:27 -0300

    Atualizando projeto RGB

--- React/Day 3/rgb/src/index.js
+++ React/Day 3/rgb/src/index.js
@@ -1,17 +1,63 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const root = document.getElementById('root');
+
+const header = (
+  <header>
+    <div className='header-wrapper'>
+      <h1>Color generator project</h1>
+      <h2>Geraldo JR</h2>
+    </div>
+  </header>
+)
+
+const hexaColor = () => {
+  let str = '0123456789abcdef';
+  let color = '';
+  for (let i = 0; i < 6; i++) {
+    let index = Math.floor(Math.random() * str.length);
+    color += str[index];
+  }
+  return '#' + color;
+};
+
+let rgb1 = hexaColor();
+let rgb2 = hexaColor();
+let rgb3 = hexaColor();
+let rgb4 = hexaColor();
+
+const cor1 = {
+  backgroundColor: rgb1,
+}
+
+const cor2 = {
+  backgroundColor: rgb2,
+}
+
+const cor3 = {
+  backgroundColor: rgb3,
+}
+
+const cor4 = {
+  backgroundColor: rgb4,
+}
+
+const Div1 = () => (
+  <div className="cores">
+    <div style={cor1} className='cor'>{rgb1}</div>
+    <div style={cor2} className='cor'>{rgb2}</div>
+    <div style={cor3} className='cor'>{rgb3}</div>
+    <div style={cor4} className='cor'>{rgb4}</div>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => (
+  <div>
+    {header}
+    <Div1 />
+  </div>
+);
+
+ReactDOM.render(<App />, root);

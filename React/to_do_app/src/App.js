d62ec7441a8b0405a9d7470e29f49f14487d4dad
Geraldo157 <gsmecanica76@gmail.com>
2023-05-15 07:31:43 -0300

    Adicionando Tailwind ao projeto

--- React/to_do_app/src/App.js
+++ React/to_do_app/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './index.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+    const [newItem, setNewItem] = useState("")
+    const [todos, setNewTodos] = useState([])
+
+    function handleSubmit(e) {
+        e.preventDefault()
+
+        setNewTodos(currentTodos =>{
+            return[
+            ...currentTodos,
+            {id: crypto.randomUUID(), title: newItem, conpleted: false},
+            ]
+        })
+        setNewItem("")
+    }
+
+    function toggleTodo(id, completed) {
+        setNewTodos(currentTodos => {
+            return currentTodos.map(todo => {
+                if(todo.id === id){
+                    return{...todo, completed}
+                }
+                return todo
+            })
+        })
+    }
+
+    function deleteTodo(id) {
+        setNewTodos(currentTodos => {
+            return currentTodos.filter(todo => todo.id !== id)
+        })
+    }
+
+    return(
+        <>
+            <form onSubmit={handleSubmit} className = "bg-yellow-400 h-40 text-center">
+                <div className="form-row">
+                    <label htmlFor="item text-3xl">New Item</label>
+                    <input value= {newItem} onChange={(e) => setNewItem(e.target.value)} type="text" id="item"/>
+                    <button className="bg-slate-700">Add</button>
+                    </div>
+            </form>
+            <h1 className="text-3xl">To do list</h1>
+            <ul className="list">
+                {todos.length === 0 && "No todos"}
+                {todos.map(todo => {
+                    return(
+                        <li id={todo.key}>
+                            <label>
+                                <input type="checkbox" 
+                                checked = {todo.completed}
+                                onChange={e => toggleTodo(todo.id, e.target.checked)} 
+                                />
+                                {todo.title}
+                            </label>
+                            <button onClick={() => deleteTodo(todo.id)} className="btn-primary">DELETE</button>
+                        </li>
+                    )
+                })}
+            </ul>
+        </>
+    )
 }
-
-export default App;
